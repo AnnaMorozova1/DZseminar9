e7eae57a8c5e593e7cc9a9aff14e2338e3d9ba90
Anna <anyutka901990@mail.ru>
2022-09-02 14:52:59 +0300

    Zadacha64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите значение M:  ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение N:  ");
+int N = int.Parse(Console.ReadLine());
+for(int i=M; i<=N; i++)
+{
+    
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+
